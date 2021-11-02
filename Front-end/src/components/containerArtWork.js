817124076653f9b5eeb4a70eb76f30374b810d92
YerGuerrero <guerreroyerlyn221113@gmail.com>
2021-11-02 01:29:13 -0600

    image tour2

--- Front-end/src/components/containerArtWork.js
+++ Front-end/src/components/containerArtWork.js
@@ -10,7 +10,7 @@ const containerArtWork = (props) => {
         <div id ="btnExhibitions" onClick={handleOnClick} className= "" style={{ background: "linear-gradient(0deg, rgba(64,41,33,1) 0%, rgba(200,147,116,1) 100%)"}}>
             <img className="imageBtnArtWork" src="/img/armadillo.png" alt="ImagenObra"/>
             <div className="titleBtnArtWork">
-                <p style={{marginBottom: 0, fontSize: "small"}}>{`Obra ${props.index +1}`}</p>
+                <p style={{marginTop: 0,marginBottom: 0, fontSize: "small"}}>{`Obra ${props.index +1}`}</p>
                 <p style={{marginTop: 0, fontWeight: 700}}>{props.title}</p>
             </div>
         </div>
